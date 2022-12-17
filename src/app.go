9ee08354e6662fdfef9af94cecf2e35822f0222c
挨踢方丈 <itabbot@qq.com>
2022-12-17 20:44:49 +0800

    添加 RequestId 中间件。

--- src/app.go
+++ src/app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/fooins/opsmgt-backend/src/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,6 +9,9 @@ import (
 func NewApp() *gin.Engine {
 	// 创建一个新的空白 Gin 实例
 	app := gin.New()
+
+	// 处理请求ID
+	app.Use(middlewares.RequestId())
 
 	// 设置路由
 	Routeing(app)
