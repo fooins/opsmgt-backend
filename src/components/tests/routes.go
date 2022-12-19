68118200ad8e677e25642898f72b14cd267f3f31
挨踢方丈 <itabbot@qq.com>
2022-12-19 22:13:34 +0800

    调整方法名。

--- src/components/tests/routes.go
+++ src/components/tests/routes.go
@@ -10,11 +10,11 @@ import (
 // 注册路由
 func RouteRegister(group *gin.RouterGroup) {
 	// 测试
-	group.GET("/test", GetTest)
+	group.GET("/test", getTest)
 }
 
 // 测试
-func GetTest(ctx *gin.Context) {
+func getTest(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"now":   time.Now(),
 		"query": ctx.Request.URL.Query(),
