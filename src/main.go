af6c0269840b26d64ec145296f02fd6f6f62c7a6
挨踢方丈 <itabbot@qq.com>
2022-12-20 21:43:22 +0800

    添加日志和错误处理。

--- src/main.go
+++ src/main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"github.com/fooins/opsmgt-backend/src/libraries/errors"
+	"github.com/fooins/opsmgt-backend/src/libraries/log"
+	"go.uber.org/zap"
+)
+
+// 获取日志记录器
+var logger = log.GetLogger(
+	"main",
+	log.SetLevelNotProd(zap.InfoLevel),
+	log.SetLevelProd(zap.InfoLevel),
+	log.SetNoErrorFile(true),
+	log.SetConsoleAll(true),
+	log.SetNoCaller(true),
+)
+
 func main() {
+	// 错误处理
+	defer func() {
+		if err := recover(); err != nil {
+			errors.HandleError(
+				errors.NormalizeError(
+					err,
+					errors.SetErrorIsTrusted(false),
+				),
+			)
+		}
+	}()
+
 	// 启动 Web 服务
+	logger.Info("Web服务启动成功")
 	StartHttpServer()
 }
